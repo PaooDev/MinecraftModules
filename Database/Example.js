7957f3fccce52503038525af0d590dafe9d73e84
PaooDev <117492984+PaooDev@users.noreply.github.com>
2022-11-05 16:45:09 +0700

    Example Database (JS)

--- Database/Example.js
+++ Database/Example.js
@@ -1,11 +1,15 @@
 import * as Database from "./Modules/Database.js"
 
 Database.set("key", "value")
-Database.get("key") // will return value
+Database.get("key") // Output : value
+Database.set("key1", "value2")
+
+Database.gets() // Output : ["key:value", "key1:value2"]
 
 // Example in Balance
-Database.set(`PlayerName_Balance`, 5000) // You can use playername to set balance
-Database.set(`Paoeni_Balance`, 1500)
+Database.set(`Balance_PlayerName`, 5000) // You can use playername to set balance
+Database.set(`Balance_Paoeni`, 1500)
 for (const player of world.getPlayers()) {
-  Database.get(`${player.nameTag}_Balance`) // will return 5000, 1500 
+  let balance = Database.get(`Balance_${player.nameTag}`)
+  console.log(`${player.nameTag} : ${balance}`)
 }
